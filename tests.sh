8d7ef4a301391e70bf6723c6919550a8e9e7662d
vinicius.hallmann@avenudecode.com <vinicius.hallmann@external.stellantis.com>
2025-12-18 12:03:33 -0300

    chore: remove unnecessary files

--- tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-make
-echo "variant,threads,N,K,chunk,time" > results_test.csv
-
-./bin/taskA 1 10000 15 1 >> results_test.csv
-./bin/taskA 2 10000 15 1 >> results_test.csv
-./bin/taskA 4 10000 15 1 >> results_test.csv
